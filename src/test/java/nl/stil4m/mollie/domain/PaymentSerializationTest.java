2eeb87b75c597cacc01714297f26f31aa07dfe7c
Michaël van de Giessen <mg@smile.nl>
2016-11-08 14:39:33 +0100

    #32 testDeserializeRecurringPaymentResponse

--- src/test/java/nl/stil4m/mollie/domain/PaymentSerializationTest.java
+++ src/test/java/nl/stil4m/mollie/domain/PaymentSerializationTest.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
@@ -36,8 +37,21 @@ public class PaymentSerializationTest {
 
         String serialized = mapper.writeValueAsString(customerPayment);
         Map mapRepresentation = mapper.readValue(serialized, Map.class);
-        InputStream resourceAsStream = this.getClass().getResourceAsStream("/expected_first_recurring_payment.json");
+        InputStream resourceAsStream = getClass().getResourceAsStream("/expected_first_recurring_payment.json");
         Map expected = mapper.readValue(resourceAsStream, Map.class);
         assertThat(mapRepresentation, is(expected));
     }
+	
+	@Test
+    public void testDeserializeRecurringPaymentResponse() throws IOException {
+		// FIXME #32 response on recurring payments contains more info; test fails if FAIL_ON_UNKNOWN_PROPERTIES is true 
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		InputStream resourceAsStream = getClass().getResourceAsStream("/response_create_recurring_payment.json");
+		
+		Payment payment = mapper.readValue(resourceAsStream, Payment.class);
+		
+		assertThat(payment.getAmount(), is(1.00));
+		assertThat(payment.getRecurringType(), is("recurring"));
+		assertThat(payment.getCustomerId(), is("cst_Kdp3uq2MeF"));
+	}
 }
